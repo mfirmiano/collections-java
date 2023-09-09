f70c78863b120a3a46a677d9b2041887796ea03b
mfirmiano <mfirmiano1902@gmail.com>
2023-09-09 08:53:03 -0300

    Testa ListaTarefa e Tarefa

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,18 @@
+package src.main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public String toString(){
+        return descricao;
+    }
+}
